bbeb558095e2b88466a111116da1874d7e690cd0
Ardra4 <146100671+Ardra4@users.noreply.github.com>
2025-11-15 15:56:01 +0530

    Update work.js

--- work.js
+++ work.js
@@ -1,9 +1,5 @@
-const designations = [
-    "  Developer",
-    " Designer",
-    "  Enthusiast"
-];
 
+const designations = ["  Developer","  Enthusiast"];
 let currentDesignationIndex = 0;
 
 function changeDesignation() {
@@ -12,12 +8,12 @@ function changeDesignation() {
 }
 window.setInterval(changeDesignation, 1000);
 
-const textArray = ["I'm a Web", "I'm a UI/UX", "I'm a Tech"];
+const textArray = ["I'm a Web", "I'm a Tech"];
 let currentTextIndex = 0;
 
 function updateText() {
-document.getElementById('designations').innerText = textArray[currentTextIndex ];
-  currentTextIndex = (currentTextIndex + 1) % textArray.length;
+    document.getElementById('designations').innerText = textArray[currentTextIndex];
+    currentTextIndex = (currentTextIndex + 1) % textArray.length;
 }
 setInterval(updateText, 1000);
 updateText();
